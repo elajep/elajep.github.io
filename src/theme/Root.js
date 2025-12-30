6218d7e7d760297d95291f99f9a77901a5645dec
Elajep <alessandro.pejretti@gmail.com>
2025-12-30 15:26:35 +0100

    Add math equation color info and update tutorial
    Updated README to list allowed CSS colors for math equations. Enhanced the EFI tutorial with colored math equations and explanations. Increased KaTeX font size in custom CSS. Minor formatting adjustment in Root.js.

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useLocation } from '@docusaurus/router';
 
+
 export default function Root({ children }) {
   const location = useLocation();
 
@@ -23,3 +24,4 @@ export default function Root({ children }) {
 
   return <>{children}</>;
 }
+
